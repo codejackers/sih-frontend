43be3d5d6276cf49ce363c95791718185307edb0
harshchandrakar <harshc252@gmail.com>
2022-07-20 11:30:17 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import HomePage from "./Components/UserFacing/HomePage";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
