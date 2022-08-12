6322fc283614dc92a83ce966b23f43823bb59e23
harshchandrakar <harshc252@gmail.com>
2022-08-12 19:26:19 +0530

    added collegeDetails component

---
+++ src/Components/DashBoard/utils/CollegeDetails.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classes from "../Style/CollegeDetails.module.css";
+import ShareIcon from "@mui/icons-material/Share";
+import LanguageIcon from "@mui/icons-material/Language";
+import CallIcon from "@mui/icons-material/Call";
+function CollegeDetails(props) {
+  return (
+    <div className={classes.CollegeDetails}>
+      <div className={classes.heading}>
+        <img src={props.clgLogo} alt="University logo" />
+        <div className={classes.desc}>
+          <h1>{props.clgName}</h1>
+          <p>{props.shortDesc}</p>
+        </div>
+      </div>
+      <div className={classes.btns}>
+        <button>
+          <ShareIcon />
+          <span>Share</span>
+        </button>
+        <button>
+          <LanguageIcon />
+          <span>College Website</span>
+        </button>
+        <button>
+          <CallIcon />
+          <span>Call</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CollegeDetails;
