dc03769534bcb7f8e5f5f2e4afd9716e59ee9094
tymsai <tymsai6076@gmail.com>
2022-08-13 16:10:55 +0530

    made changes in  2nd card in college description

--- src/Components/DashBoard/utils/CollegeDetails2.js
+++ src/Components/DashBoard/utils/CollegeDetails2.js
@@ -1,16 +1,12 @@
 import React from "react";
 import classes from "../Style/CollegeDetails2.module.css";
 import PinDropIcon from "@mui/icons-material/PinDrop";
-function CollegeDetails(props) {
+function CollegeDetails2(props) {
   return (
     <div className={classes.CollegeDetails}>
       <div className={classes.desc}>
         <div className={classes.desc}>
-          <p>{props.longDesc}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </p>
-          <h5>{props.clgAddr}</h5>
+          <p>{props.longDesc}</p>
         </div>
       </div>
       <div className={classes.btns}>
@@ -18,12 +14,10 @@ function CollegeDetails(props) {
           <PinDropIcon/>
           <span>Locate on Maps</span>
         </button>
-        <p className={classes.clgAddr}>
-        -College Address-
-        </p>
+        <p>{props.clgAddress}</p>
       </div>
     </div>
   );
 }
 
-export default CollegeDetails;
+export default CollegeDetails2;
