f30c1e4586cc34095d637ad262d3ad9d0131e9b4
Div-develops <divyapawar.sstc@gmail.com>
2022-08-13 17:43:15 +0530

    added otp verification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import "./App.css";
+<<<<<<< Updated upstream
 import HomePage from "./Components/UserFacing/HomePage";
 import Login from "./Components/hei/Login";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+=======
+import Login from "./components/hei/Login";
+import Registration from "./components/Registration/Registration";
+import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import React from 'react'
+import HomePage from "./components/UserFacing/HomePage";
+import About from "./components/about/utils/About";
+import Otp from "./components/hei/utils/Otp";
+
+
+
+>>>>>>> Stashed changes
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
           <Route path="/hei/Login" element={<Login />} />
+          <Route path="/hei/utils/Otp" element={<Otp/>}/> 
+          <Route path="/about" element={<About />} />
+
         </Routes>
+
+        
       </div>
+<<<<<<< Updated upstream
+=======
+      
+>>>>>>> Stashed changes
     </Router>
   );
 }
---
+++ src/Components/about/utils/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "../Style/AboutPage.module.css";
+
+
+  
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+  
+export default About;
