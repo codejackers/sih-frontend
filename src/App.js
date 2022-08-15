6629b9a765a41072662a919345df939a5996de9e
Div-develops <divyapawar.sstc@gmail.com>
2022-08-15 17:56:30 +0530

    solved conflicts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import "./App.css";
-<<<<<<< Updated upstream
-import HomePage from "./Components/UserFacing/HomePage";
-import Login from "./Components/hei/Login";
+
+import HomePage from "./components/UserFacing/HomePage";
+import Login from "./components/hei/Login";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-=======
-import Login from "./components/hei/Login";
-import Registration from "./components/Registration/Registration";
-import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
-import React from 'react'
-import HomePage from "./components/UserFacing/HomePage";
+
+
+import React from "react";
+
 import About from "./components/about/utils/About";
-import Otp from "./components/hei/utils/Otp";
 
 
-
->>>>>>> Stashed changes
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
           <Route path="/hei/Login" element={<Login />} />
-          <Route path="/hei/utils/Otp" element={<Otp/>}/> 
+          
           <Route path="/about" element={<About />} />
-
+          
         </Routes>
-
-        
       </div>
-<<<<<<< Updated upstream
-=======
-      
->>>>>>> Stashed changes
     </Router>
   );
 }
