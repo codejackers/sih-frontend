017781de5cf3681a7d5d3f185c9a334f57a98919
harshchandrakar <harshc252@gmail.com>
2022-08-15 13:54:13 +0530

    added new setup for uploading document

--- src/Components/hei/utils/DocUpload.js
+++ src/Components/hei/utils/DocUpload.js
@@ -1,20 +1,35 @@
 import NavBar from "../../UserFacing/utils/Navbar";
 import React, { useState } from "react";
-import MenuButton from "../../UserFacing/utils/MenuButton";
 import classes from "../Style/DocUpload.module.css";
 import { useDispatch } from "react-redux";
-import { registerCollege, registerUID } from "../../../actions/auth";
+import { addFile, registerCollege, registerUID } from "../../../actions/auth";
 import SucccessPopup from "./SucccessPopup";
 import { useNavigate } from "react-router-dom";
 import { PinDropSharp } from "@mui/icons-material";
+import S3FileUpload from "react-s3";
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
+const S3_BUCKET = process.env.REACT_APP_S3_BUCKET;
+const REGION = process.env.REACT_APP_REGION;
+const ACCESS_KEY = process.env.REACT_APP_AWS_ACCESS_KEY;
+const SECRET_ACCESS_KEY = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+
+const config = {
+  bucketName: S3_BUCKET,
+  region: REGION,
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_ACCESS_KEY,
+  dirName: "photos",
+};
 function DocUpload(props) {
   const [UID, setUID] = useState("");
   const [open, setOpen] = useState(false);
+  const [doc, setDoc] = useState("");
   let navigate = useNavigate();
   const dispatch = useDispatch();
   const handleNext = () => {
     if (UID != "") {
-      dispatch(registerUID({ uid: UID, doc: "ssd" }));
+      dispatch(registerUID({ uid: UID, doc: doc }));
       setTimeout(() => {
         dispatch(registerCollege());
       }, 100);
@@ -24,6 +39,15 @@ function DocUpload(props) {
   const handleClose = () => {
     navigate("/");
   };
+
+  const handleUpload = async (file) => {
+    S3FileUpload.uploadFile(file, config)
+      .then((data) => {
+        setDoc(data.location);
+      })
+      .catch((err) => console.error(err));
+  };
+  console.log(doc);
   return (
     <div className={classes.mainDiv}>
       <NavBar name="Registration" />
@@ -43,13 +67,22 @@ function DocUpload(props) {
               }}
               placeholder="University ID"
               className={classes.inpId}
+              required
             />
             <div className={classes.uploadDocFlex}>
               <p className={classes.infoDoc}>Supporting Documents</p>
               <label htmlFor="files" className={classes.label}>
                 Upload Files
               </label>
-              <input id="files" className={classes.fileUpload} type="file" />
+              <input
+                id="files"
+                className={classes.fileUpload}
+                onChange={(e) => {
+                  handleUpload(e.target.files[0]);
+                }}
+                type="file"
+                required
+              />
             </div>
             <button type="button" className={classes.btn} onClick={handleNext}>
               Send Registration Request
