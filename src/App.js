e55f4b7a8ffdef8fb4ea4e420503fd2a1c4ae8e1
tymsai <tymsai6076@gmail.com>
2022-08-17 08:03:40 +0530

    updated popup edit details dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import HomePage from "./Components/UserFacing/HomePage";
 import Login from "./Components/hei/Login";
-import EditDetails from "./Components/DashBoard/utils/EditDetails";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -11,7 +10,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/EditDetails" element={<EditDetails open={true}/>} />
 
           <Route path="/hei/Login" element={<Login />} />
         </Routes>
