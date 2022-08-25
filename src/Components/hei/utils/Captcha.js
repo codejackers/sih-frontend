fc29beae77c3e4ca872f130fd95c3825eb695e9d
harshchandrakar <harshc252@gmail.com>
2022-08-25 18:33:36 +0530

    added reCaptcha

---
+++ src/Components/hei/utils/Captcha.js
@@ -0,0 +1,14 @@
+// import React, { useRef } from "react";
+// import classes from "../Style/Captcha.module.css";
+// import reCAPTCHA from "react-google-recaptcha";
+// function Captcha() {
+//   const TEST_SITE_KEY = "6LeQZKghAAAAAGUX_vX2CCVwltV5eoB7PJwefavB";
+//   const captchaRef = useRef(null);
+//   return (
+//     <div className={classes.Captcha}>
+//       <reCAPTCHA sitekey={TEST_SITE_KEY} ref={captchaRef} />
+//     </div>
+//   );
+// }
+
+// export default Captcha;
